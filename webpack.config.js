68da8672654e38319ebd99229b21ae663e93c435
Sapphireyed <sapphireyed@gmail.com>
2021-09-08 12:48:39 +0200

    finished project

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,8 @@ module.exports = {
 
     output: {
         filename: '[name].[contenthash]bundle.js',
-        //path: path.resolve(__dirname, 'deploy'),
-          path: '/artGroup/',
+        path: path.resolve(__dirname, 'deploy'),
+        //  path: '/artGroup/',
     },
 
     devServer: {
